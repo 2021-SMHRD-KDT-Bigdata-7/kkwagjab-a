bd37d8ef53646ddb275c3163796d97e0247b16ba
smhrd <smhrd@DESKTOP-LPFLOAE>
2021-10-12 17:41:01 +0900

    여섯번째 커몬~

--- GitTest/src/com/gitTest/Test04.java
+++ GitTest/src/com/gitTest/Test04.java
@@ -3,7 +3,7 @@ package com.gitTest;
 public class Test04 {
 
 	public static void main(String[] args) {
-		System.out.println("ÇöÁßÀÌ Ä¸¼õ±Ãµ­ÀÌ");
+		System.out.println("ÇöÁßÀÌ Ä¹¼õ ±Ãµ­ÀÌ");
 	}
 
 }
